22fb9375f6c69c2e2f7812855253b177e8d79fe7
thongdanghoang <thongdh3401@gmail.com>
2024-05-25 21:51:19 +0700

    SWD-18: correct styling

--- frontend/src/modules/shared/components/footer/AppFooter.tsx
+++ frontend/src/modules/shared/components/footer/AppFooter.tsx
@@ -1,73 +1,77 @@
+import logo from '@assets/images/logo.svg';
 import './AppFooter.scss';
 import {ReactElement} from 'react';
-import logo from '@assets/images/logo.svg';
 
 export default function AppFooter(): ReactElement {
   return (
     <>
       <div className="app-footer">
-        <footer className="page-footer font-small blue pt-4">
-          <div className="container-fluid ">
-            <div className="row">
-              <div className="col-md-6 mt-md-0 mt-3">
-                <div className="footer-content">
-                  <h5 className="text-uppercase">
-                    <img src={logo} alt="" />
-                  </h5>
-                  <p>
-                    Morbi cursus porttitor enim lobortis molestie. Duis gravida
-                    turpis dui, eget bibendum magna congue nec.
-                  </p>
-                  <h1>(212)555-0114 hoặc swapme@customerservice.com</h1>
+        <div className="container">
+          <div className="page-footer">
+            <div className="d-flex justify-content-between my-5">
+              <div className="app-info d-flex flex-column gap-2">
+                <h5 className="text-uppercase">
+                  <img src={logo} alt="" />
+                </h5>
+                <p className="app-description regular-14">
+                  Swap Me là nơi trao đổi với mục đích tạo ra cho bạn một kênh
+                  trung gian giúp kết nối giữa người mua với người bán một cách
+                  dễ dành, nhanh chóng và tiện lợi.
+                </p>
+                <div className="CTA d-flex justify-content-between gap-3">
+                  <a className="semibold-14" href="tel:2125550114">
+                    Hotline: (212)555-0114
+                  </a>
+                  <p className="regular-14">hoặc</p>
+                  <a
+                    className="semibold-14"
+                    href="mailto:swapme@customerservice.com"
+                  >
+                    swapme@customerservice.com
+                  </a>
                 </div>
               </div>
 
-              <hr className="clearfix w-100 d-md-none pb-0" />
-
-              <div className="col-md-3 mb-md-0 mb-3">
-                <ul className="list-unstyled text-end">
-                  <h5 className="text-uppercase">Hỗ trợ khách hàng</h5>
+              <div className="app-support d-flex gap-5">
+                <ul className="support list-unstyled text-end d-flex flex-column gap-3 me-5">
+                  <h5 className="semibold-16">Hỗ trợ khách hàng</h5>
                   <li>
-                    <a>Trung tâm trợ giúp</a>
+                    <a className="regular-14">Trung tâm trợ giúp</a>
                   </li>
                   <li>
-                    <a>An toàn mua bán</a>
+                    <a className="regular-14">An toàn mua bán</a>
                   </li>
                   <li>
-                    <a>Liên hệ hỗ trợ</a>
+                    <a className="regular-14">Liên hệ hỗ trợ</a>
                   </li>
                 </ul>
-              </div>
 
-              <div className="col-md-2 mb-md-0 mb-3">
-                <ul className="list-unstyled text-end text-success">
-                  <h5 className="text-uppercase">Về chúng tôi</h5>
+                <ul className="help list-unstyled text-end d-flex flex-column gap-3">
+                  <h5 className="semibold-16">Về chúng tôi</h5>
                   <li>
-                    <a>Giới thiệu</a>
+                    <a className="regular-14">Giới thiệu</a>
                   </li>
                   <li>
-                    <a>Quy chế hoạt động</a>
+                    <a className="regular-14">Quy chế hoạt động</a>
                   </li>
                   <li>
-                    <a>Chính sách bảo mật</a>
+                    <a className="regular-14">Chính sách bảo mật</a>
                   </li>
                   <li>
-                    <a>Giải quyết tranh chấp</a>
+                    <a className="regular-14">Giải quyết tranh chấp</a>
                   </li>
                 </ul>
               </div>
             </div>
           </div>
-
-          <div className="line">
-            <hr />
-          </div>
-        </footer>
-        <div className="d-flex justify-content-between pb-3">
-          <div className="footer-versionapp">Swapme Version 12.03.02</div>
-          <div className="footer-location">
-            Địa chỉ: Đại học FPT _ E2a-7, Đường D1, Phường Long Thạnh Mỹ, Q9,
-            Thành phố Thủ Đức, TP.HCM
+          <div className="copyright d-flex justify-content-between">
+            <div className="footer-version-app regular-14">
+              Swapme Version 12.03.02
+            </div>
+            <div className="footer-location regular-14">
+              Địa chỉ: Đại học FPT - E2a-7, Đường D1, Phường Long Thạnh Mỹ, Q9,
+              Thành phố Thủ Đức, TP.HCM.
+            </div>
           </div>
         </div>
       </div>
