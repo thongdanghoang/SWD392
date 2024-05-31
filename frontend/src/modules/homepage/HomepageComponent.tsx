41c596743238bb3caf6f646da94f35969438f002
phuccoder <anhdeptraippt@gmail.com>
2024-05-31 22:38:07 +0700

    SWD-25: Homepage ProductList

--- frontend/src/modules/homepage/HomepageComponent.tsx
+++ frontend/src/modules/homepage/HomepageComponent.tsx
@@ -1,10 +1,12 @@
 import './Homepage.scss';
 import {ReactElement} from 'react';
+import ProductList from '../shared/components/productlist/ProductList';
 
 export default function HomepageComponent(): ReactElement {
   return (
     <div className="homepage">
       <h1>Homepage worked!</h1>
+      <ProductList />
     </div>
   );
 }
