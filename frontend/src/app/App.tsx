c726340d2f0392ce310e669a1b0b86ba57215770
dano <thong.danghoang@elca.vn>
2024-05-31 12:47:33 +0700

    SWD-37: setup react router

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import {AuthContextProps, useAuth} from 'oidc-react';
+import {ReactElement} from 'react';
+import ApplicationService from '../modules/shared/services/application.service.ts';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import TestLoginComponent from '../modules/dev/TestLoginComponent.tsx';
 import AppHeader from '../modules/shared/components/header/AppHeader.tsx';
 import AppFooter from '../modules/shared/components/footer/AppFooter.tsx';
-import {ReactElement} from 'react';
-import ApplicationService from '../modules/shared/services/application.service.ts';
+import HomepageComponent from '../modules/homepage/HomepageComponent.tsx';
 
 export default function App(): ReactElement {
   const auth: AuthContextProps = useAuth();
@@ -12,10 +14,21 @@ export default function App(): ReactElement {
   applicationService.setAuth(auth);
 
   return (
-    <div className="app">
-      <AppHeader />
-      <TestLoginComponent />
-      <AppFooter />
-    </div>
+    <Router>
+      <div className="app">
+        <div className="header">
+          <AppHeader />
+        </div>
+        <div className="body">
+          <Routes>
+            <Route path="/" element={<HomepageComponent />}></Route>
+            <Route path="/dev" element={<TestLoginComponent />}></Route>
+          </Routes>
+        </div>
+        <div className="footer">
+          <AppFooter />
+        </div>
+      </div>
+    </Router>
   );
 }
