0f6137b95c9727278a7456084cb9b5f3de68de4e
Nguyễn Thục Ngân <nganntqe170236@fpt.edu.vn>
2024-07-02 17:38:49 +0700

    SWD-68: pagination for homapage search and search bar work

--- frontend/src/modules/shared/models/model.ts
+++ frontend/src/modules/shared/models/model.ts
@@ -1,43 +1,16 @@
-export function defaultPagination(offset = 0, limit = 20): SearchPageDto {
-  return {offset, limit};
-}
-
-export function defaultSort(
-  colId: string = 'id',
-  sort: SortDirection = SortDirection.ASC
-): SortDto {
-  return {colId, sort};
-}
-
-export function defaultSearchCriteria<C>(
-  criteria: C,
-  page: SearchPageDto = defaultPagination(),
-  sort: SortDto = defaultSort()
-): SearchCriteriaDto<C> {
-  return {criteria, page, sort};
-}
-
 export interface SearchPageDto {
   offset: number;
   limit: number;
 }
-
 export interface SortDto {
   colId: string;
-  sort: SortDirection;
+  sort: string;
 }
-
-export enum SortDirection {
-  ASC = 'asc',
-  DESC = 'desc'
-}
-
 export interface SearchCriteriaDto<C> {
   page: SearchPageDto;
   criteria: C;
   sort: SortDto;
 }
-
 export interface SearchResultDto<R> {
   results: R[];
   total: number;
