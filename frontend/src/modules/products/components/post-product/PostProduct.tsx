83f1c816493520ecae16e80fb4b4c16ea2daba8e
thaintd <114746768+thaintd@users.noreply.github.com>
2024-07-08 19:38:32 +0700

    SWD-44

--- frontend/src/modules/products/components/post-product/PostProduct.tsx
+++ frontend/src/modules/products/components/post-product/PostProduct.tsx
@@ -114,11 +114,6 @@ export default function PostProduct(): React.ReactElement {
   const handleUploadVideoComplete = (videoUrl: string): void => {
     setProduct(prevProduct => ({...prevProduct, video: videoUrl}));
   };
-<<<<<<< HEAD
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-=======
-
->>>>>>> d92c416 (SWD-44)
   useEffect(() => {}, [product]);
 
   return (
@@ -179,13 +174,8 @@ export default function PostProduct(): React.ReactElement {
               as="select"
               required
               className="list-of-postings form-select semibold-16 text-color-tertiary"
-<<<<<<< HEAD
-              value={product.category} // Ensure this reflects the current category ID in your product state
-              onChange={e => setProduct({...product, category: e.target.value})} // Update the product state with the selected category ID
-=======
               value={product.category}
               onChange={e => setProduct({...product, category: e.target.value})}
->>>>>>> d92c416 (SWD-44)
             >
               {categories.map(category => (
                 <option key={category.id} value={category.id}>
