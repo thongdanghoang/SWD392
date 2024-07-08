e5a93676f28732afb7116f781ec8303cb34d4c8d
thaintd <114746768+thaintd@users.noreply.github.com>
2024-07-08 20:01:10 +0700

    SWD-44

--- backend/src/modules/category/category.service.ts
+++ backend/src/modules/category/category.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
 import {CategoryEntity} from './category.entity';
+import {CategoryDto} from './category.dto';
 
 @Injectable()
 export class CategoryService {
@@ -10,7 +11,15 @@ export class CategoryService {
     readonly categoryRepository: Repository<CategoryEntity>
   ) {}
 
-  async findAll(): Promise<CategoryEntity[]> {
-    return this.categoryRepository.find();
+  async findAll(): Promise<CategoryDto[]> {
+    const categories = await this.categoryRepository.find();
+    return categories.map(category => {
+      const dto = new CategoryDto();
+      dto.id = category.id;
+      dto.version = category.version;
+      dto.title = category.title;
+      dto.image = category.image;
+      return dto;
+    });
   }
 }
