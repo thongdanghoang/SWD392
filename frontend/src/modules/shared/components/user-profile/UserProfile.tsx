bf5334e01c9822342f45145ea662daeb095068d8
thaintd <114746768+thaintd@users.noreply.github.com>
2024-07-17 08:44:15 +0700

    swd105

--- frontend/src/modules/shared/components/user-profile/UserProfile.tsx
+++ frontend/src/modules/shared/components/user-profile/UserProfile.tsx
@@ -1,15 +1,50 @@
-import {ReactElement, useContext, useState} from 'react';
+import {ReactElement, useContext, useState, useEffect} from 'react';
 import './UserProfile.scss';
 import {UserDto} from '../../models/userDto';
 import {useNavigate} from 'react-router-dom';
-import UserProduct from './UserProduct';
 import AppButton from '../buttons/AppButton.tsx';
 import {UserContext} from '../../services/userContext.ts';
+import {
+  ProductStatus,
+  ProductWithOwnerDTO
+} from '../../../homepage/model/productWithOwnerDTO.ts';
+import {formatDistanceToNow} from 'date-fns';
+import {vi} from 'date-fns/locale';
+import {useApplicationService} from '../../services/application.service.ts';
+import {AppRoutingConstants} from '../../app-routing.constants.ts';
+import {formatToVietnameseCurrency} from '../../utils.ts';
 
 export default function UserProfile(): ReactElement {
   const currentUser: UserDto | null | undefined = useContext(UserContext)?.user;
   const [activeTab, setActiveTab] = useState<'selling' | 'sold'>('selling');
   const navigate = useNavigate();
+  const applicationService = useApplicationService();
+
+  const navigateToDetail = (id: string) => {
+    return (): void => {
+      navigate(`/products/${id}`);
+    };
+  };
+  const [myProducts, setMyProducts] = useState<ProductWithOwnerDTO[]>([]);
+  useEffect((): void => {
+    if (applicationService.isAuthenticated()) {
+      applicationService
+        .createApiClient()
+        .get(AppRoutingConstants.MY_PRODUCTS_PATH)
+        .then(response => {
+          setMyProducts(response.data.data ?? []);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }, [applicationService.isAuthenticated()]);
+  const sellingProducts = myProducts.filter(
+    product => product.status === ('PUBLISHED' as ProductStatus)
+  );
+  const soldProducts = myProducts.filter(
+    product => product.status === ('EXCHANGED' as ProductStatus)
+  );
 
   // Function to handle tab click
   const handleTabClick = (tab: 'selling' | 'sold'): void => {
@@ -87,12 +122,109 @@ export default function UserProfile(): ReactElement {
 
       {/* Conditional Rendering */}
       <div>
-        {activeTab === 'selling' ? (
-          <UserProduct />
-        ) : (
+        {activeTab === 'selling' && (
           <div>
-            <div className="regular-14 text-color-quaternary">
-              Bạn chưa bán được sản phẩm nào.
+            <div className="d-flex gap-4 flex-column">
+              <div className="exchange-info d-flex gap-3">
+                {sellingProducts.length > 0 ? (
+                  <div className="my-products d-flex justify-content-start gap-3">
+                    {sellingProducts?.map((product: ProductWithOwnerDTO) => (
+                      <li
+                        key={product.id}
+                        className="product-card clickable"
+                        onClick={navigateToDetail(product.id)}
+                      >
+                        <div className="product-image">
+                          <img src={product?.images?.[0]} alt={product.title} />
+                        </div>
+                        <div className="product-info mt-2">
+                          <div className="d-flex flex-column align-items-start">
+                            <div className="product-title text-color-tertiary semibold-16">
+                              {product.title}
+                            </div>
+                            <p className="product-price text-color-quaternary semibold-20">
+                              {product?.isGiveAway
+                                ? 'Cho tặng miễn phí'
+                                : formatToVietnameseCurrency(
+                                    product?.suggestedPrice
+                                  )}
+                            </p>
+                            <p className="product-creation-date text-color-tertiary regular-12">
+                              Đăng cách đây{' '}
+                              {formatDistanceToNow(
+                                new Date(product.creationDate),
+                                {
+                                  addSuffix: true,
+                                  locale: vi
+                                }
+                              )}{' '}
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </div>
+                ) : (
+                  <div className="my-products d-flex justify-content-center align-items-center">
+                    <div className="regular-14 text-color-quaternary">
+                      Người này chưa có sản phẩm nào để giao dịch
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+        {activeTab === 'sold' && (
+          <div>
+            <div className="d-flex gap-4 flex-column">
+              <div className="exchange-info d-flex gap-3">
+                {soldProducts.length > 0 ? (
+                  <div className="my-products d-flex justify-content-start gap-3">
+                    {soldProducts?.map((product: ProductWithOwnerDTO) => (
+                      <li
+                        key={product.id}
+                        className="product-card clickable"
+                        onClick={navigateToDetail(product.id)}
+                      >
+                        <div className="product-image">
+                          <img src={product?.images?.[0]} alt={product.title} />
+                        </div>
+                        <div className="product-info mt-2">
+                          <div className="d-flex flex-column align-items-start">
+                            <div className="product-title text-color-tertiary semibold-16">
+                              {product.title}
+                            </div>
+                            <p className="product-price text-color-quaternary semibold-20">
+                              {product?.isGiveAway
+                                ? 'Cho tặng miễn phí'
+                                : formatToVietnameseCurrency(
+                                    product?.suggestedPrice
+                                  )}
+                            </p>
+                            <p className="product-creation-date text-color-tertiary regular-12">
+                              Đăng cách đây{' '}
+                              {formatDistanceToNow(
+                                new Date(product.creationDate),
+                                {
+                                  addSuffix: true,
+                                  locale: vi
+                                }
+                              )}{' '}
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </div>
+                ) : (
+                  <div className="my-products d-flex justify-content-center align-items-center">
+                    <div className="regular-14 text-color-quaternary">
+                      Người này chưa bán sản phẩm nào.
+                    </div>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         )}
