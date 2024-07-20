29414c21721e6fbe44fa5110979a847d8c979b77
Nguyễn Thục Ngân <nganntqe170236@fpt.edu.vn>
2024-07-20 09:59:50 +0700

    SWD-109: MIGRATE prod data as sample data to local

--- backend/src/resources/migrations/SWD-44.sql
+++ backend/src/resources/migrations/SWD-44.sql
@@ -1,10 +1,3 @@
-CREATE TABLE `swapme`.`category` (
-                                         `id` INT NOT NULL,
-                                         `version` INT NOT NULL,
-                                         `title` VARCHAR(255) NOT NULL,
-                                         `image` TEXT,
-                                         PRIMARY KEY (`id`)
-);
 INSERT INTO category (id, version, title, image) VALUES (1, 1, 'Thời trang nam', 'https://res.cloudinary.com/dgkth0wxx/image/upload/v1719997958/image_1_l3evib.png');
 INSERT INTO category (id, version, title, image) VALUES (2, 1, 'Thời trang nữ', 'https://res.cloudinary.com/dgkth0wxx/image/upload/v1719997958/image_1_1_tlxxvb.png');
 INSERT INTO category (id, version, title, image) VALUES (3, 1, 'Giày dép', 'https://res.cloudinary.com/dgkth0wxx/image/upload/v1719997957/image_1_2_n0zjhf.png');
@@ -17,4 +10,3 @@ INSERT INTO category (id, version, title, image) VALUES (9, 1, 'Sách', 'https:/
 INSERT INTO category (id, version, title, image) VALUES (10, 1, 'Văn phòng phẩm', 'https://res.cloudinary.com/dgkth0wxx/image/upload/v1719997957/image_1_7_vjkr6o.png');
 INSERT INTO category (id, version, title, image) VALUES (11, 1, 'Giải trí', 'https://res.cloudinary.com/dgkth0wxx/image/upload/v1719998324/image_1_11_z5o5kd.png');
 INSERT INTO category (id, version, title, image) VALUES (12, 1, 'Thể thao', 'https://res.cloudinary.com/dgkth0wxx/image/upload/v1719999034/image_1_9_nvjhns.png');
-DROP TABLE `swapme`.`category`;
